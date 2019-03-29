4b945f5ad8180901c1a5d69c7ba65858fba6d344
sabar <sabartanjungsari@gmail.com>
2019-03-29 09:49:31 +0700

    "First Commit"

---
+++ src/test/java/org/sabar/easynote/EasyNotesApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sabar.easynote;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasyNotesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
