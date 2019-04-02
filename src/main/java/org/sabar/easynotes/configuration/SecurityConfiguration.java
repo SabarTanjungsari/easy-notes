0a2c26eda91087383f7e07ede291053f05197f2b
sabar <sabartanjungsari@gmail.com>
2019-04-02 12:20:32 +0700

    Security configuration

--- src/main/java/org/sabar/easynotes/configuration/SecurityConfiguration.java
+++ src/main/java/org/sabar/easynotes/configuration/SecurityConfiguration.java
@@ -1,5 +1,45 @@
 package org.sabar.easynotes.configuration;
 
-public class SecurityConfiguration {
+import org.sabar.easynotes.exception.LoggingAccessDeniedHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private LoggingAccessDeniedHandler accessDeniedHandler;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/js/**", "/css/**", "/images/**", "/fonts/**").permitAll()
+				.antMatchers("/user/**").hasRole("USER").anyRequest().authenticated().and().formLogin()
+				.defaultSuccessUrl("/", true).loginPage("/login").permitAll().and().logout()
+				.invalidateHttpSession(true).clearAuthentication(true)
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout")
+				.permitAll().and().exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				/**/
+				.withUser("user").password(passwordEncoder().encode("password")).roles("USER")
+				/**/
+				.and()
+				/**/
+				.withUser("manager").password(passwordEncoder().encode("password")).roles("MANAGER");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
