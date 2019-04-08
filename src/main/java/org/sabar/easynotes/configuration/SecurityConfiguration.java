0951480388fb602d4a35432ce95fb3c4d51c8464
sabar <sabartanjungsari@gmail.com>
2019-04-08 15:24:36 +0700

    Access Menu By Role Setting #sec:authorize="hasRole('ROLE_USER')"

--- src/main/java/org/sabar/easynotes/configuration/SecurityConfiguration.java
+++ src/main/java/org/sabar/easynotes/configuration/SecurityConfiguration.java
@@ -11,47 +11,39 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private LoggingAccessDeniedHandler accessDeniedHandler;
+    @Autowired
+    private LoggingAccessDeniedHandler accessDeniedHandler;
 
-	@Autowired
-	private DataSource dataSource;
+    @Autowired
+    private DataSource dataSource;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				/* Permit ALL */
-				.antMatchers("/", "/js/**", "/css/**", "/images/**", "/fonts/**").permitAll()
-				/* Access USER Role */
-				.antMatchers("/user/**").hasRole("ADMIN").anyRequest().authenticated()
-				/* Login Rule */
-				.and().formLogin().defaultSuccessUrl("/", true).loginPage("/login").permitAll()
-				/* Logout Rule */
-				.and().logout().invalidateHttpSession(true).clearAuthentication(true)
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout")
-				.permitAll()
-				/* Handling */
-				.and().exceptionHandling().accessDeniedHandler(accessDeniedHandler);
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource)
-				.authoritiesByUsernameQuery("SELECT usr.name AS username, rl.name AS role "
-						+ "FROM ad_user usr JOIN user_role ur ON ur.ad_user_id = usr.ad_user_id "
-						+ "JOIN ad_role rl ON rl.ad_role_id = ur.ad_role_id "
-						+ "WHERE usr.active = 'Y' AND rl.active = 'Y'AND usr.name=?")
-				.usersByUsernameQuery(
-						"select name AS username, password, active as enabled  from ad_user WHERE active = 'Y' AND name=?");
-	}
+        http.csrf().disable().authorizeRequests().antMatchers("/js/**", "/css/**", "/images/**", "/fonts/**")
+                .permitAll().antMatchers("/admin/**").hasAnyRole("ADMIN").antMatchers("/user/**").hasAnyRole("USER")
+                .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+                .permitAll().and().exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .authoritiesByUsernameQuery("SELECT usr.name AS username, rl.name AS role "
+                        + "FROM ad_user usr JOIN user_role ur ON ur.ad_user_id = usr.ad_user_id "
+                        + "JOIN ad_role rl ON rl.ad_role_id = ur.ad_role_id "
+                        + "WHERE usr.active = 'Y' AND rl.active = 'Y'AND usr.name=?")
+                .usersByUsernameQuery(
+                        "select name AS username, password, active as enabled  from ad_user WHERE active = 'Y' AND name=?");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
--- src/main/java/org/sabar/easynotes/controller/HomeController.java
+++ src/main/java/org/sabar/easynotes/controller/HomeController.java
@@ -6,23 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping("/")
-	public String root() {
-		return "index";
-	}
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
 
-	@GetMapping("/admin")
-	public String userIndex() {
-		return "admin/index";
-	}
+    @GetMapping("/admin")
+    public String userIndex() {
+        return "admin/index";
+    }
 
-	@GetMapping("/login")
-	public String login() {
-		return "login";
-	}
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 
-	@GetMapping("/access-denied")
-	public String accessDenied() {
-		return "/error/access-denied";
-	}
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "/error/access-denied";
+    }
 }
